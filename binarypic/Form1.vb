c9daa2d4dc084804ca33aa6aa36660e212f6a991
Stan13y <46853837+iamstan13y@users.noreply.github.com>
2021-05-24 23:56:45 +0200

    Initial commit

---
+++ binarypic/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim pic As [Byte]()
+        Dim mstream As New IO.MemoryStream
+        Dim image As New Bitmap(PictureBox1.Image)
+        image.Save(mstream, Imaging.ImageFormat.Jpeg)
+        pic = mstream.ToArray
+        mstream.Close()
+    End Sub
+End Class
